6d3d1b2a3a3ac5578f81536893c2b96d32bdf47a
lucas <lucassilveira07.ls@gmail.com>
2023-10-17 09:55:45 -0300

    exercicio

---
+++ exercícios/lista 2/ex07.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float cateto_adj, cateto_opos;
+	printf("Digite o valor do cateto adjacente: ");
+	scanf("%f", &catetoadj);
+	printf("Digite o valor do cateto oposto: ");
+	scanf("%f", &cateto_opos);
+	hipotenusa = 
+	
+}
+
+int quadrado(float num){
+	num *= num;
+	return num;
+			
+	}
